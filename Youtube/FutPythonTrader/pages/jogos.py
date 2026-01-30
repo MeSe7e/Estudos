7078f11526a3b8a23ea009142bc61bd3e11e8c48
MeSe7e <antimio62@sapo.pt>
2026-01-30 16:33:58 +0000

    Update jogos.py

--- Youtube/FutPythonTrader/pages/jogos.py
+++ Youtube/FutPythonTrader/pages/jogos.py
@@ -5,4 +5,10 @@ import numpy as np
 import datetime
 from datetime import date
 
+# Título da página
 st.title("Jogos do Dia")
+
+# Para escolher o dia que queremos ver dos jogos do dia
+dia = st.date_input(
+    "Data de Análise,
+    date.today())"
